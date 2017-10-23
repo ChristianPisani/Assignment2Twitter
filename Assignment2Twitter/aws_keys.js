e811fcc002ae1bb0ea6495d6fdbf4329adfad70f
Christian Thorvik <christian96-th@hotmail.com>
2017-10-23 22:24:18 +1000

    La inn støtte for persistence. Ikke sikker hvordan dette skal brukes enda

---
+++ Assignment2Twitter/aws_keys.js
@@ -0,0 +1,5 @@
+const access_key = "AKIAJGK7DYHCBMJCVZOA";
+const secret = "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";
+
+exports.access_key = access_key;
+exports.secret = secret;
--- Assignment2Twitter/routes/index.js
+++ Assignment2Twitter/routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var aws_keys = require('../aws_keys');
+var aws = require('aws-sdk');
+
+const bucket = "tweetbucketcab432";
+
+//Filestream
+const fs = require("fs");
+
+aws.config.update({
+    accessKeyId: aws_keys.access_key,
+    secretAccessKey: aws_keys.secret
+});
+
+var s3 = new aws.S3();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Twitter Search' });
@@ -19,6 +34,44 @@ router.get('/tweets', function(req, res) {
 
 router.get("/visualization", function(req, res) {
   res.render('datavisualization', {title: 'Data'});
+});
+
+router.get("/persistence", function(req, res) {
+  var params = {
+    Bucket: bucket,
+    Key: "tweetkey.txt"
+  };
+  s3.getObject(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.write("<h1>An error occurred</h1>");
+      res.end();
+    } else {
+      res.write(data.Body);
+      res.end();
+    }
+  });
+});
+
+router.post("/persistence", function(req, res) {
+  var params = {
+    Body: "Test Twitter Text",
+    Bucket: bucket,
+    Key: "tweetkey.txt",
+    ServerSideEncryption: "AES256",
+    Tagging: "key1=value1&key2=value2"
+  };
+  console.log(params);
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      res.end();
+    } else {
+      console.log(data);
+      res.json(data);
+      res.end();
+    }
+  });
 });
 
 module.exports = router;
