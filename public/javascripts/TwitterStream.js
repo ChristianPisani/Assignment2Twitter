d41406902e1cc389bb3e36198bffee0cb9d26114
Christian Thorvik <christian96-th@hotmail.com>
2017-10-25 21:05:36 +1000

    Analyserer tweets om trump på /visualization \n Dette gjøres på klient, så analyseringen må flyttes til server

---
+++ public/javascripts/TwitterStream.js
@@ -0,0 +1,19 @@
+script(src="https://code.jquery.com/jquery-3.1.1.min.js") //JQuery
+script(src="https://d3js.org/d3.v4.min.js") //D3
+
+function FetchTwitterStream(keywords) {
+  $.post(`/twitterstream/${lastindex}`, {keywords: keywords},
+    function(res) {
+      lastindex = res.lastIndex;
+
+      var tweetsDiv = d3.select('#stream');
+
+      const tweets = res.tweets;
+      
+      for(i = 0; i < tweets.length; i++) {
+        tweetsDiv.append('p').text(tweets[i].text);
+      }
+  });
+}
+
+exports.FetchTwitterStream = FetchTwitterStream;
